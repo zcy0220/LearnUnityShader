a5fff1af37086c4c331bd82e7a7c7a0729b41bc2
晨杨 郑 <690574486@qq.com>
2022-01-11 00:02:53 +0800

    渲染Cubemap工具

--- Assets/Editor/RenderCubemapWizard.cs
+++ Assets/Editor/RenderCubemapWizard.cs
@@ -1,18 +1,29 @@
-using System.Collections;
 using UnityEngine;
 using UnityEditor;
 
 public class RenderCubemapWizard : ScriptableWizard
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform RenderFromPosition;
+    public Cubemap ExportCubemap;
+    
+    [MenuItem("Tools/Render Cubemap")]
+    public static void Test()
     {
-        
+        ScriptableWizard.DisplayWizard<RenderCubemapWizard>(
+            "Render Cubemap", "Render");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnWizardUpdate()
     {
-        
+        isValid = (RenderFromPosition != null && ExportCubemap != null);
+    }
+
+    private void OnWizardCreate()
+    {
+        GameObject go = new GameObject("CubemapCamera");
+        go.AddComponent<Camera>();
+        go.transform.position = RenderFromPosition.position;
+        go.GetComponent<Camera>().RenderToCubemap(ExportCubemap);
+        DestroyImmediate(go);
     }
 }
