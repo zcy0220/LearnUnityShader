9ceb0f08b80760a0f1ee4e7b8403e0a279789a91
chenyang <690574486@qq.com>
2022-01-10 13:50:23 +0800

    立方体纹理

---
+++ Assets/Editor/RenderCubemapWizard.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+public class RenderCubemapWizard : ScriptableWizard
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
